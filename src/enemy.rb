e07646a72a762cc1d86d88a059b87e695a7f6b73
ameliaxjones <gcab022111@coderacademy.edu.au>
2021-12-04 12:19:56 +1000

    added classes for leeroy and enemy

---
+++ src/enemy.rb
@@ -0,0 +1,9 @@
+require_relative("./leeroy.rb")
+
+class Enemy < Leeroy
+    def initialize(name, hp, attack)
+        super(attack)
+        super(hp)
+        @name = name
+    end
+end
---
+++ src/leeroy.rb
@@ -0,0 +1,10 @@
+
+
+class Leeroy 
+    def initialize (hp, attack, block, run)
+        @hp = hp
+        @attack = attack
+        @block = block
+        @run = run
+    end
+end
